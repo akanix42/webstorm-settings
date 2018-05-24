5db01b2edf984d20fe77aafc6cde1b393b6b5ec5
Nathan Reid <nathantreid@DESKTOP-340DTOT>
2018-05-24 08:36:47 -0500

    WS-2018.1.4 <nathantreid@DESKTOP-340DTOT Create baseRefactoring.xml, vim_settings.xml, TS Jest༏ext༏.unit.test.ts, ide.general.xml, git.xml, Obsidian copy.icls, project.default.xml, diff.xml, Default copy.xml, other.xml, Obsidian.xml, Default.xml, file.template.settings.xml, code.style.schemes, find.xml, ui.lnf.xml, filetypes.xml, scratch_config.xml, keymap.xml, colors.scheme.xml, External Tools.xml, _@user_Darcula.icls, Material Theme - Default copy1.icls, Vue File.vue, github_settings.xml, bitbucket-server-registry.xml, Default.xml, laf.xml, material_custom_theme.xml, Material Theme - Default copy.icls, web-browsers.xml, macros.xml, material_theme.xml, vcs.xml, bitbucket-cloud-registry.xml, editor.codeinsight.xml, Material Theme - Default copy2.icls, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/TS Jest༏ext༏.unit.test.ts
@@ -0,0 +1,8 @@
+describe('${Unit_under_test}', () => {
+  it('should ', () => {
+    #[[$END$]]#
+    const expectedOutput = '';
+    const output = '';
+    expect(output).toEqual(expectedOutput);
+  });
+});
